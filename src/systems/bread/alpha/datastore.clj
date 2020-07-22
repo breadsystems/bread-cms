c59b2a4e105c0279c956baacc430154ab807eb6e
Coby Tamayo <acobster@gmail.com>
2020-07-22 10:33:01 -0700

    move connect! defn to top for clarity/hierarchy

--- src/systems/bread/alpha/datastore.clj
+++ src/systems/bread/alpha/datastore.clj
@@ -1,6 +1,17 @@
 (ns systems.bread.alpha.datastore)
 
 
+(defmulti connect! :datastore/type)
+
+(defmethod connect! :default [{:datastore/keys [type] :as config}]
+  (let [msg (if (nil? type)
+              "No :datastore/type specified in datastore config!"
+              (str "Unknown :datastore/type `" type "`!"
+                   " Did you forget to load a plugin?"))]
+    (throw (ex-info msg {:config config
+                         :bread.context :datastore/connect!}))))
+
+
 (defprotocol KeyValueDatastore
   (get-key [store k])
   (set-key [store k v])
@@ -33,14 +44,3 @@
 
 (defprotocol TransactionalDatastoreConnection
   (transact [conn timepoint]))
-
-
-(defmulti connect! :datastore/type)
-
-(defmethod connect! :default [{:datastore/keys [type] :as config}]
-  (let [msg (if (nil? type)
-              "No :datastore/type specified in datastore config!"
-              (str "Unknown :datastore/type `" type "`!"
-                   " Did you forget to load a plugin?"))]
-    (throw (ex-info msg {:config config
-                         :bread.context :datastore/connect!}))))
