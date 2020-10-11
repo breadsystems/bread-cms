98122a0f48be2ff3698977462d48ba27123eeb59
Coby Tamayo <acobster@gmail.com>
2020-10-10 17:43:52 -0700

    more experimental static site gen stuff

---
+++ dev/breadbox/static.clj
@@ -0,0 +1,88 @@
+(ns breadbox.static
+  (:require
+   [clojure.instant :as instant]
+   [clojure.string :as string]
+   [clojure.java.io :as io]
+   [markdown.core :as md]
+   [systems.bread.alpha.core :as bread]
+   [systems.bread.alpha.datastore :as d]
+   [systems.bread.alpha.templates :as tpl])
+  (:import
+   [systems.bread.alpha.datastore BreadStore]))
+
+(defn- write-post! [dir slug {:keys [content]}]
+  (spit (str dir java.io.File/separator slug ".html") content))
+
+(deftype FileSystemStore [opts]
+  BreadStore
+  (d/-type->posts [this type]
+    ;; TODO
+    [])
+  (d/-slug->post [this slug]
+    {:slug slug
+     ;; TODO cljs impl?
+     :content (some-> (str (:src opts) java.io.File/separator slug (:extension opts))
+                      (io/resource)
+                      slurp)})
+  (d/-update-post! [this slug post]
+    (write-post! (:dest opts) slug post))
+  ;; -add-post! -delete-post!
+  )
+
+(comment
+  (write-post! "dist"
+               "hey"
+               {:slug "hey"
+                :content "Hey you, out there in the cold..."}))
+
+(defn persist-post
+  "Query for and set :post in the request context"
+  [req]
+  (let [datastore (d/datastore req)
+        post (d/slug->post datastore :default (bread/hook req :slug))]
+    (-> req
+        ;; Produce a raw response from the post content
+        (bread/response {:body (:content post)})
+        ;; Persist the post data directly
+        (bread/add-value-hook :post post))))
+
+(defn generate-sitemap* [handler opts])
+
+(defn static-site-plugin [{:keys [src dest extension renderer]}]
+  ;; TODO
+  ;; * compute sitemap
+  ;; * compile each route in sitemap
+  ;; * eventually: model dependency tree and re-compile in real time
+  (fn [req]
+    (let [datastore-opts {:src src
+                          :dest dest
+                          :extension (or extension ".md")}
+          template (tpl/renderer->template (or renderer md/md-to-html-string))]
+      (-> req
+          (d/set-datastore (FileSystemStore. datastore-opts))
+          ;; TODO get this from the router?
+          (bread/add-value-hook :slug (string/replace (:url req) #"/" ""))
+          (bread/add-hook :hook/dispatch persist-post)
+          (bread/add-hook :hook/render template {:precedence 0})
+          #_(bread/add-hook :hook/decorate (fn [{:keys [body] :as res}]
+                                           (d/update-post! res (bread/hook res :slug) {:content body})
+                                           res))))))
+
+(comment
+  (instant/read-instant-date "2020-10-25T20:59:36+00:00")
+  (inst-ms (java.util.Date.)))
+
+(defn- sitemap-entry-handler [handler]
+  ;; TODO rerender lazily based on lastmod
+  (fn [{:keys [loc _lastmod]}]
+    (handler {:url loc})))
+
+(defn generator [handler]
+  (let [entry->response (sitemap-entry-handler handler)]
+    (fn [sitemap]
+      (map entry->response sitemap))))
+
+(defn generate! [handler _opts]
+  (let [gen (generator handler)]
+    (gen [{:loc "/one" :lastmod #inst "2020-09-25T20:59:36+00:00"}
+          {:loc "/two" :lastmod #inst "2020-10-08T20:59:36+00:00"}])))
---
+++ dev/pages/one.md
@@ -0,0 +1,17 @@
+## ONE!!
+
+One is the loneliest number that you'll ever doooo.
+
+> This is a block quote
+
+**This stuff is bold** and *this is italicized!* This is just regular type but `here is some code`.
+
+![Placeholder image](https://placehold.it/500x300)
+
+Irure proident sit cumque voluptas nihil veniam. Eu repellendus animi in pariatur placeat aliquip. Labore dignissimos minim excepturi atque. Enim quod distinctio corrupti vero corrupti. Officia at nam atque. Anim dolorum consectetur magna placeat iusto. Pariatur aliqua excepturi et, optio nam reprehenderit quod labore fugiat do in. Vero ullamco atque enim placeat, mollitia ullamco aute quod labore animi.
+
+Non cupidatat nulla cupidatat magna do. Cupiditate excepturi odio quas consectetur consectetur, aliquip deleniti non deserunt. Cupidatat placeat mollit minim sunt distinctio cupiditate, lorem sint laboris cillum. Quis nostrud harum ducimus occaecat sed maxime minim, velit do facilis eos animi adipisicing dolorum. Dolorum placeat cum voluptate similique voluptatum amet molestias, esse nisi quas dolor dolore mollit consectetur. Mollit quod assumenda dolor. Dolor consectetur anim harum in sed. Dolorum corrupti deleniti minus incididunt eu.
+
+Magna et labore maxime sint nam. Aliqua anim soluta nulla impedit aliqua assumenda, cum incididunt ex sunt eos eu voluptas. At sed et quis ea praesentium rerum. Quos libero ipsum reprehenderit incididunt. Quidem sint laboris aliquip velit quas harum, laborum repellendus in quos cum provident. Deleniti velit eu mollitia officia laborum veniam, duis placeat deserunt labore non. Nostrud occaecat amet minus quos ullamco eos, quod harum dolorum nostrud.
+
+Cumque cupiditate amet distinctio maxime aliquip. Tempore molestias mollit deleniti, assumenda incididunt provident proident fugiat voluptatum deserunt. Nulla mollit voluptas mollit in, ex quos exercitation veniam officia ea. Excepturi nostrud libero magna blanditiis commodo eiusmod minus, proident quas est consectetur occaecat voluptate. Ea exercitation nihil aute atque libero, impedit laboris ex nobis ut. Quis labore eu eiusmod culpa qui.
