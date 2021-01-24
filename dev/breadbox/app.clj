4354d6f69cd527505dfd512b2591cebe75fe22f7
Coby Tamayo <acobster@gmail.com>
2021-01-23 18:24:22 -0800

    simplify breadbox

--- dev/breadbox/app.clj
+++ dev/breadbox/app.clj
@@ -9,16 +9,8 @@
    [ring.middleware.reload :refer [wrap-reload]]
    [rum.core :as rum :exclude [cljsjs/react cljsjs/react-dom]]))
 
-
-(comment
-  (defn fsroot->app [root]
-    (-> {:plugins [(d/store->plugin (FileSystemStore. root))]}
-        bread/app
-        bread/load-plugins))
-  (d/slug->post (fsroot->app ".") :type/post "one"))
-
 (def handler (-> {:plugins [;; TODO logging plugin
-                            #_(tpl/response->plugin
+                            (tpl/response->plugin
                                {:headers {"Content-Type" "text/html"}
                                 :body [:html
                                        [:head
@@ -26,18 +18,6 @@
                                         [:meta {:charset "utf-8"}]]
                                        [:body
                                         [:div.bread-app [:h1 "Hello, Breadster!"]]]]})
-                            (static/static-site-plugin {:src "pages"
-                                                        :dest "dist"})
-                            (tpl/renderer->plugin (fn [body]
-                                                    ;; TODO i18n
-                                                    [:html {:lang "en"}
-                                                     [:head
-                                                      ;; TODO SEO
-                                                      [:title "Main Title"]]
-                                                     [:body
-                                                      [:main
-                                                       [:h1 "Main Title"]
-                                                       [:article (tpl/dangerous body)]]]]))
                             (tpl/renderer->plugin rum/render-static-markup {:precedence 2})]}
                  (bread/app)
                  (bread/app->handler)))
@@ -49,52 +29,16 @@
   ;; TODO test this out!
   (static/generate! handler)
 
-  (defn translate-string [s] s)
-  ;; TODO ideas for i18n
-  ;; Dispatch on content field vs. hard-coded transation keys in markup...
-  (def html [:html {:lang "en"}
-             [:head
-              [:title :text/hello]]
-             [:body
-              [:main
-               [:h1 :text/hello]
-               [:h2 :text/invalid]
-               [:article (tpl/dangerous (translate-string "Some rich text content..."))]]]])
-
-  ;; Here we delegate to tempura or similar
-  ;; https://github.com/ptaoussanis/tempura
-  (defn tr [x]
-    (let [dict {:missing "MISSING"
-                :hello "Hello"}]
-      (get dict x (:missing dict))))
-
-  (defn i18n [x]
-    ;; TODO
-    (if (and (keyword? x) (= "text" (namespace x)))
-      (tr (keyword (name x)))
-      x))
-
-  (clojure.walk/postwalk i18n html)
-
-  (defn profiler-for-hooks [hooks f]
-    (fn [call]
-      (when (contains? hooks (:hook call)) (f call))))
-
-  (defn prn-keys [ks m]
-    (prn (select-keys m ks)))
-
-  (binding [bread/*hook-profiler* (profiler-for-hooks #{:hook/render} (partial prn-keys [:hook :args]))]
-    (handler {:url "one"})
-    (slurp "dist/one.html"))
-  (:body (handler {:url "two"})))
-
+  ;;
+  )
 
 (defonce stop-http (atom nil))
 
 (defn start! []
+  ;; TODO config
   (let [port (Integer. (or (System/getenv "HTTP_PORT") 8080))]
     (println (str "Running HTTP server at localhost:" port))
-    (reset! stop-http (http/run-server (wrap-reload handler) {:port port})))
+    (reset! stop-http (http/run-server (wrap-reload #'handler) {:port port})))
   nil)
 
 (defn stop! []
@@ -107,15 +51,9 @@
   :start (start!)
   :stop  (stop!))
 
-
-(defn -main [& _args]
-  (mount/start))
-
-
 (defn restart []
   (mount/stop #'http-server)
   (mount/start #'http-server))
-
 
 (comment
   (mount/stop #'http-server)
