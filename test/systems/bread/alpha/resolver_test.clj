f7a0d152420ac9ffb1edc694289bd1aa393989d3
Coby Tamayo <acobster@gmail.com>
2021-08-06 10:50:09 -0700

    rm test for dead API feature

--- test/systems/bread/alpha/resolver_test.clj
+++ test/systems/bread/alpha/resolver_test.clj
@@ -5,23 +5,6 @@
     [systems.bread.alpha.component :refer [defc]]
     [systems.bread.alpha.resolver :as resolver]))
 
-(defc with-basic-pull [_]
-  {:query [:post/title :post/slug]
-   :key :basic}
-  [:p "whatever"])
-
-(defc with-multi-pull [_]
-  {:query {'?e [:post/title :post/slug]
-           '?fields [:field/key :field/content]}
-   :key :multi}
-  [:p "whatever"])
-
-(deftest test-query-key
-  (are [k resolver] (= k (resolver/query-key resolver))
-
-    :basic {:resolver/component with-basic-pull}
-    :multi {:resolver/component with-multi-pull}))
-
 (deftest test-pull-query
   (are [clause resolver] (= clause (-> resolver
                                        resolver/pull-query
