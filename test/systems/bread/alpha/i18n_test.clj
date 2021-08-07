509905978876941384418a4d53102e37dcbf7cae
Coby Tamayo <acobster@gmail.com>
2021-08-07 00:49:18 -0700

    fix i18n tests

--- test/systems/bread/alpha/i18n_test.clj
+++ test/systems/bread/alpha/i18n_test.clj
@@ -1,6 +1,7 @@
 (ns systems.bread.alpha.i18n-test
   (:require
     [clojure.test :refer [deftest is testing use-fixtures]]
+    [kaocha.repl :as k]
     [systems.bread.alpha.core :as bread]
     [systems.bread.alpha.datastore :as store]
     [systems.bread.alpha.i18n :as i18n]
@@ -12,10 +13,10 @@
               :store {:backend :mem
                       :id "i18n-db"}
               :datastore/initial-txns
-              [#:i18n{:key :i18n/one :string "One" :lang :en}
-               #:i18n{:key :i18n/two :string "Two" :lang :en}
-               #:i18n{:key :i18n/one :string "Uno" :lang :es}
-               #:i18n{:key :i18n/two :string "Dos" :lang :es}]}
+              [#:i18n{:key :one :string "One" :lang :en}
+               #:i18n{:key :two :string "Two" :lang :en}
+               #:i18n{:key :one :string "Uno" :lang :es}
+               #:i18n{:key :two :string "Dos" :lang :es}]}
       app #(h/plugins->loaded [(store/plugin config)
                                (i18n/plugin)])]
 
@@ -26,16 +27,20 @@
                         (f)
                         (store/delete-database! config)))
 
+  (deftest test-supported-langs
+    (is (= #{:en :es} (i18n/supported-langs (app)))))
+
   (deftest test-lang
     (is (= :en (i18n/lang ((bread/handler (app)) {:uri "/en"}))))
     (is (= :en (i18n/lang ((bread/handler (app)) {:uri "/en/qwerty"}))))
     (is (= :en (i18n/lang ((bread/handler (app)) {:uri "/"}))))
     (is (= :en (i18n/lang ((bread/handler (app)) {:uri "/qwerty"}))))
-    #_
     (is (= :es (i18n/lang ((bread/handler (app)) {:uri "/es"}))))
-    (is (= :fr (i18n/lang ((bread/handler (app)) {:uri "/fr"})))))
+    ;; No :fr in database: defaults to :en
+    (is (= :en (i18n/lang ((bread/handler (app)) {:uri "/fr"})))))
 
-  #_
   (deftest test-strings-for
+    (is (= {:one "Uno" :two "Dos"} (i18n/strings-for (app) :es)))))
 
-    (is (= {:one "Uno" :two "Dos"} (bread/strings-for )))))
+(comment
+  (k/run))
