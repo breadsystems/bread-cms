969c186e7a73dbe284a17eda21ad32ed4e1fea9b
Coby Tamayo <acobster@gmail.com>
2021-12-14 01:36:54 -0800

    migrate diff UI to its own ns for readability

--- tools/systems/bread/alpha/tools/debug.cljs
+++ tools/systems/bread/alpha/tools/debug.cljs
@@ -1,19 +1,15 @@
 (ns systems.bread.alpha.tools.debug
   (:require
     [clojure.edn :as edn]
-    [datascript.core :as d]
     [editscript.core :as ed]
     [rum.core :as rum]
     [systems.bread.alpha.tools.debug.db :as db :refer [db]]
     [systems.bread.alpha.tools.debug.event :as e]
     [systems.bread.alpha.tools.debug.request :as r]
     [systems.bread.alpha.tools.util :refer [date-fmt
-                                            date-fmt-ms
-                                            join-some
-                                            pp
-                                            req->url
-                                            shorten-uuid]]
-    [systems.bread.alpha.tools.diff :as diff]))
+                                            #_
+                                            pp]]
+    [systems.bread.alpha.tools.debug.diff :as diff]))
 
 (defonce !ws (atom nil))
 
@@ -22,86 +18,14 @@
     ;; TODO transit
     (.send ws (prn-str msg))))
 
-(defn- uuid->req [uuid]
-  (get-in @db [:request/uuid uuid]))
-
-(defn- uuid->max-tx [uuid]
-  (-> uuid uuid->req (get-in [:request/response :response/datastore :max-tx])))
-(defn- idx->req [idx]
-  (get @db/requests (get @db/req-uuids idx)))
-
 (defn clear-debug-log! []
   (when (js/confirm "Clear all debug data? This cannot be undone.")
     (send! [:clear-debug-log])
     (reset! db db/initial)))
 
-(defn- diff-entities [[a b] diff-type]
-  (when (and a b)
-    (condp = diff-type
-      :response-pre-render
-      [(get-in (uuid->req a) [:response/pre-render])
-       (get-in (uuid->req b) [:response/pre-render])]
-      :database
-      [(get-in (uuid->req a) [:request/response :response/datastore])
-       (get-in (uuid->req b) [:request/response :response/datastore])])))
-
-(defn- diff-uuid-options [uuid]
-  (map (fn [req-uuid]
-         [req-uuid (shorten-uuid db/req-uuid)])
-       (filter #(not= uuid %) @db/req-uuids)))
-
-(rum/defc diff-line [n line]
-  (let [attrs {:key n :data-line (inc n)}]
-    (if (string? line)
-      [:pre.str attrs line]
-      (let [[op line] line]
-        (condp = op
-          :- [:pre.del attrs line]
-          :+ [:pre.add attrs line]
-          :gap [:pre (assoc attrs :style {:margin-top "1em"}) line])))))
-
-(rum/defc diff-ui < rum/reactive []
-  (let [current-uuid (rum/react db/req-uuid)
-        ;; What kind of diff is the user viewing?
-        diff-type (rum/react db/diff-type)
-        ;; Get each UUID in the diff.
-        [ua ub] (rum/react db/diff-uuids)
-        ;; Get the timestamp for each response being diffed.
-        ;; We use this info to detect if a diff is oriented
-        ;; reverse-chronologically, and, if so, to indicate that to the user.
-        [ta tb] (mapv uuid->max-tx [ua ub])
-        [source target] (diff-entities [ua ub] diff-type)
-        [ra rb script] (diff/diff-struct-lines source target)]
-    [:article.rows
-     [:header.rows
-      [:h2 "Diff: " [:code (shorten-uuid ua)] " → " [:code (shorten-uuid ub)]]
-      [:div.flex
-       [:button {:on-click #(swap! db assoc :ui/diff nil)}
-        "← Back to " (shorten-uuid current-uuid)]
-       [:button {:on-click #(swap! db update :ui/diff reverse)}
-        "↺ Reverse diff"]
-       [:select
-        {:value (name diff-type)
-         :on-change #(swap! db assoc :ui/diff-type
-                            (keyword (.. % -target -value)))}
-        [:option {:value "response-pre-render"} "Response (pre-render)"]
-        [:option {:value "database"} "Database"]]]
-      (when (> ta tb)
-        [:p.info
-         "This diff is in reverse-chronological order."
-         " The data on the left is older than the data on the right."])]
-     #_
-     (map-indexed (fn [idx [path op value]]
-            [:pre {:key idx} (str path) " " (name op) " " (pp value)])
-          (ed/get-edits script))
-     [:div.diff
-      [:div.response.diff-source
-       (map-indexed (fn [idx line] (diff-line idx line)) ra)]
-      [:div.response.diff-target
-       (map-indexed (fn [idx line] (diff-line idx line)) rb)]]]))
-
 (rum/defc ui < rum/reactive []
-  (let [reqs (map uuid->req (rum/react db/req-uuids))
+  (let [reqs (map #(get-in @db [:request/uuid %])
+                  (rum/react db/req-uuids))
         selected (rum/react db/selected)
         loading? (rum/react db/loading?)
         diff (rum/react db/diff-uuids)
@@ -156,7 +80,7 @@
                    :on-change #(swap! db update :ui/replay-as-of? not)}]
           [:label {:for "pref-replay-as-of"} "Replay with " [:code "as-of"]]]]]
        (cond
-         diff (diff-ui)
+         diff (diff/diff-ui)
          current-uuid (r/request-details)
          (seq reqs) [:p "Click a request to view details"]
          :else [:p.info "No requests yet."])]]
