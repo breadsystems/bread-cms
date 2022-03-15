645f77e01b33663e0f3b93dec1321ec0d2e436ab
Coby Tamayo <acobster@gmail.com>
2022-03-14 22:42:27 -0700

    add build tools

---
+++ build.clj
@@ -0,0 +1,23 @@
+(ns build
+  (:require
+    [clojure.tools.build.api :as b]))
+
+(def lib 'systems.bread/bread-core)
+(def version (format "0.5.%s" (b/git-count-revs nil)))
+(def class-dir "target/classes")
+(def basis (b/create-basis {:project "deps.edn"}))
+(def jar-file (format "target/%s-%s.jar" (name lib) version))
+
+(defn clean [_]
+  (b/delete {:path "target"}))
+
+(defn jar [_]
+  (b/write-pom {:class-dir class-dir
+                :lib lib
+                :version version
+                :basis basis
+                :src-dirs ["src"]})
+  (b/copy-dir {:src-dirs ["src"]
+               :target-dir class-dir})
+  (b/jar {:class-dir class-dir
+          :jar-file jar-file}))
