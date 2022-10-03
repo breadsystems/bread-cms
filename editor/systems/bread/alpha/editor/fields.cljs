5a8f9d18eac3bd3f35ecf16983b4a518122846cc
Coby Tamayo <coby@tamayo.email>
2022-10-02 17:55:08 -0700

    impl :update-state

--- editor/systems/bread/alpha/editor/fields.cljs
+++ editor/systems/bread/alpha/editor/fields.cljs
@@ -7,11 +7,22 @@
     (js/console.error (str "No init-field! multimethod defined for: "
                            (prn-str (:type config))))))
 
+(defn- ->dom-event [event-key]
+  ;; TODO get this to work for camelCased events like mouseDown.
+  ;; See React's impl:
+  ;; https://github.com/facebook/react/blob/17806594cc28284fe195f918e8d77de3516848ec/packages/react-dom/src/events/DOMEventProperties.js#L120-125
+  (subs (name event-key) 3))
+
+(comment
+  (->dom-event :on-click))
+
 (defmethod core/init-field! :repeater
   [ed element {:keys [each] :as config}]
   (when each
-    (let [{:keys [on-click]} (:events each)
-          ;; TODO more event handlers
-          ]
-      (doseq [child (.querySelectorAll element (:selector each))]
-        (core/listen! ed child "click" (fn [e] (core/event! ed e child on-click)))))))
+    (doseq [child (.querySelectorAll element (:selector each))]
+      (doseq [[event-key event-spec] (:events each)]
+        (let [handler (if (sequential? event-spec)
+                        (fn [e] (doseq [event event-spec]
+                                  (core/event! ed e child event)))
+                        (fn [e] (core/event! ed e child event-spec)))]
+          (core/listen! ed child (->dom-event event-key) handler))))))
