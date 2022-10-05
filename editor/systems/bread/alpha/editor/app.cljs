09c65c19ad22bad9cdb1b74c5fd800ba01e25308
Coby Tamayo <coby@tamayo.email>
2022-10-05 00:48:46 -0700

    collaborative cursors

--- editor/systems/bread/alpha/editor/app.cljs
+++ editor/systems/bread/alpha/editor/app.cljs
@@ -7,22 +7,38 @@
 (defonce ydoc (Y/Doc.))
 
 (defonce my-editor
-  (atom (editor/editor {:name :my-editor
-                        :site/name "Sandbox"
-                        :bar/mount-into "#editor-bar"
-                        :bar/sections [:site-name
-                                       :settings
-                                       {:type :media-library
-                                        :label "Media"}
-                                       :spacer
-                                       :save-button]
-                        :collab {:strategy :webrtc
-                                 :ydoc ydoc
-                                 :provider
-                                 (WebrtcProvider.
-                                   "breadroom" ydoc
-                                   (clj->js {:signaling
-                                             ["ws://localhost:4444"]}))}})))
+  (let [config (editor/from-meta-element)]
+    (atom (editor/editor {:name :my-editor
+                          :site/name "Sandbox"
+                          :bar/mount-into "#editor-bar"
+                          :bar/sections [:site-name
+                                         :settings
+                                         {:type :media-library
+                                          :label "Media"}
+                                         :spacer
+                                         :save-button]
+                          :collab {:strategy :webrtc
+                                   :ydoc ydoc
+                                   :provider
+                                   (WebrtcProvider.
+                                     "breadroom" ydoc
+                                     (clj->js {:signaling
+                                               ["ws://localhost:4444"]}))
+                                   #_#_
+                                   :user (:collab/user config)
+                                   :user {:name (rand-nth ["cobby"
+                                                           "rown"
+                                                           "yous"
+                                                           "joshy"
+                                                           "willy"
+                                                           "jacko"])
+                                          :color (rand-nth ["red"
+                                                            "blue"
+                                                            "green"
+                                                            "grey"
+                                                            "brown"
+                                                            "yellow"])
+                                          :data-avatar "/cat.jpg"}}}))))
 
 (defn ^:dev/after-load start []
   (editor/init! my-editor {}))
