2c52f26678a984bd4db280cb9061c4d149a7a382
Coby Tamayo <coby@tamayo.email>
2022-10-04 23:36:26 -0700

    GET COLLAB WORKING OVER RTC!!!!

--- editor/systems/bread/alpha/editor/app.cljs
+++ editor/systems/bread/alpha/editor/app.cljs
@@ -1,6 +1,10 @@
 (ns systems.bread.alpha.editor.app
   (:require
+    ["yjs" :as Y]
+    ["y-webrtc" :refer [WebrtcProvider]]
     [systems.bread.alpha.editor.api :as editor]))
+
+(defonce ydoc (Y/Doc.))
 
 (defonce my-editor
   (atom (editor/editor {:name :my-editor
@@ -11,7 +15,14 @@
                                        {:type :media-library
                                         :label "Media"}
                                        :spacer
-                                       :save-button]})))
+                                       :save-button]
+                        :collab {:strategy :webrtc
+                                 :ydoc ydoc
+                                 :provider
+                                 (WebrtcProvider.
+                                   "breadroom" ydoc
+                                   (clj->js {:signaling
+                                             ["ws://localhost:4444"]}))}})))
 
 (defn ^:dev/after-load start []
   (editor/init! my-editor {}))
