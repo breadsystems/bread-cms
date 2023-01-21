a828dc835c388eb51c4489689254fea447c885e1
Coby Tamayo <coby@tamayo.email>
2023-01-20 16:18:59 -0800

    move components into their own ns

--- dev/breadbox/app.clj
+++ dev/breadbox/app.clj
@@ -2,7 +2,9 @@
 ;; ...which is to say, all of them.
 (ns breadbox.app
   (:require
+    [breadbox.components :as c]
     [breadbox.data :as data]
+
     [clojure.core.protocols :refer [Datafiable]]
     [clojure.datafy :refer [datafy]]
     [clojure.edn :as edn]
@@ -12,7 +14,6 @@
     [kaocha.repl :as k]
     [systems.bread.alpha.defaults :as defaults]
     [systems.bread.alpha.core :as bread]
-    [systems.bread.alpha.component :as component :refer [defc]]
     [systems.bread.alpha.datastore :as store]
     [systems.bread.alpha.plugin.datahike :as dh]
     [systems.bread.alpha.i18n :as i18n]
@@ -61,100 +62,6 @@
               :datastore/initial-txns
               data/initial-content})
 
-(declare main-nav)
-
-(defc main-layout
-  [{:keys [main-content i18n menus lang]}]
-  {:content-path [:main-content]}
-  [:html {:lang lang}
-   [:head
-    [:title {:breadbox i18n}]]
-   [:body
-    [:header
-     (main-nav (:main-nav menus))]
-    [:main main-content]
-    [:footer
-     (main-nav (:footer-nav menus))]]])
-
-(defn main-nav [menu]
-  [:nav {:class (:my/class menu)}
-   [:ul
-    (map
-      (fn [{:keys [url title children]}]
-        [:li
-         [:a {:href url} title]
-         (when (seq children)
-           [:ul
-            (map
-              (fn [{:keys [url title]}]
-                [:li
-                 [:a {:href url} title]])
-              children)])])
-      (:items menu))]])
-
-(defc home [{:keys [post menus] :as x}]
-  {:extends main-layout
-   :query [:post/slug {:post/fields [:field/key :field/content]}]
-   :key :post}
-  (let [post (post/compact-fields post)
-        {:keys [title simple]} (:post/fields post)]
-    [:<>
-     [:h1 title]
-     [:p (:hello simple)]
-     [:img {:src (:img-url simple)}]]))
-
-(defc category-page [{:keys [taxon i18n menus] :as data}]
-  {:extends main-layout
-   :query [:taxon/slug
-           {:taxon/fields [:field/key :field/content]}
-           {:post/_taxons
-            [:post/slug {:post/fields [:field/key :field/content]}]}]
-   :key :taxon}
-  (let [{:taxon/keys [slug fields posts]} taxon]
-    [:<>
-     [:h1 (:title fields)]
-     [:div [:code slug]]
-     (map
-       (fn [{:post/keys [slug fields]}]
-         [:article
-          [:h2 (:title fields)]])
-       posts)]))
-
-(defc page [{:keys [post i18n menus] :as data}]
-  {:extends main-layout
-   :query [{:post/fields [:field/key :field/content]}]
-   :key :post}
-  (let [post (post/compact-fields post)
-        {:keys [title simple flex-content]} (:post/fields post)]
-    [:<>
-     [:h1 title]
-     [:h2 (:hello simple)]
-     [:p (:body simple)]
-     [:p.goodbye (:goodbye simple)]
-     [:p.flex flex-content]]))
-
-(defc static-page [{:keys [post lang]}]
-  {:key :post}
-  [:html {:lang lang}
-   [:head
-    [:title (first (:title post))]
-    [:meta {:charset "utf-8"}]]
-   [:body
-    [:main
-     [:div (when (:html post)
-             {:dangerouslySetInnerHTML
-              {:__html (:html post)}})]]]])
-
-(defc ^:not-found not-found [{:keys [i18n lang]}]
-  {}
-  ;; TODO extract this to a layout
-  [:html {:lang lang}
-   [:head
-    [:title (:not-found i18n)]
-    [:meta {:charset "utf-8"}]]
-   [:body
-    [:div (:not-found i18n)]]])
-
 (defn hello-handler [_]
   {:body "Hello!"
    :status 200
@@ -174,22 +81,22 @@
       ;; TODO :bread/component -> :dispatcher/component etc.
       ["/" {:name :bread.route/home
             :bread/dispatcher :dispatcher.type/page
-            :bread/component home
+            :bread/component c/home
             :bread/cache
             {:param->attr {:lang :field/lang}
              :pull [{:post/fields [:lang]}]}}]
       ["/static/:slug/" {:bread/dispatcher :dispatcher.type/static
-                         :bread/component static-page
+                         :bread/component c/static-page
                          :bread/watch-static {:dir "dev/content"
                                               :path->req [0 "static" 1]}
                          :name :bread.route/static}]
       ["/cat/:slug/" {:bread/dispatcher {:dispatcher/type :dispatcher.type/taxon
                                          :taxon/taxonomy :taxon.taxonomy/category
                                          :dispatcher/key :posts}
-                     :bread/component category-page}]
+                     :bread/component c/category-page}]
       ["/*slugs" {:name :bread.route/page
                   :bread/dispatcher :dispatcher.type/page
-                  :bread/component page
+                  :bread/component c/page
                   :bread/cache
                   {:param->attr {:slugs :post/slug :lang :field/lang}
                    :pull [:slugs {:post/fields [:lang]}]}}]]]
---
+++ dev/breadbox/components.cljc
@@ -0,0 +1,98 @@
+(ns breadbox.components
+  (:require
+    [systems.bread.alpha.component :refer [defc]]
+    [systems.bread.alpha.post :as post]))
+
+(declare main-nav)
+
+(defc main-layout
+  [{:keys [main-content i18n menus lang]}]
+  {:content-path [:main-content]}
+  [:html {:lang lang}
+   [:head
+    [:title {:breadbox i18n}]]
+   [:body
+    [:header
+     (main-nav (:main-nav menus))]
+    [:main main-content]
+    [:footer
+     (main-nav (:footer-nav menus))]]])
+
+(defn main-nav [menu]
+  [:nav {:class (:my/class menu)}
+   [:ul
+    (map
+      (fn [{:keys [url title children]}]
+        [:li
+         [:a {:href url} title]
+         (when (seq children)
+           [:ul
+            (map
+              (fn [{:keys [url title]}]
+                [:li
+                 [:a {:href url} title]])
+              children)])])
+      (:items menu))]])
+
+(defc home [{:keys [post menus] :as x}]
+  {:extends main-layout
+   :query [:post/slug {:post/fields [:field/key :field/content]}]
+   :key :post}
+  (let [post (post/compact-fields post)
+        {:keys [title simple]} (:post/fields post)]
+    [:<>
+     [:h1 title]
+     [:p (:hello simple)]
+     [:img {:src (:img-url simple)}]]))
+
+(defc category-page [{:keys [taxon i18n menus] :as data}]
+  {:extends main-layout
+   :query [:taxon/slug
+           {:taxon/fields [:field/key :field/content]}
+           {:post/_taxons
+            [:post/slug {:post/fields [:field/key :field/content]}]}]
+   :key :taxon}
+  (let [{:taxon/keys [slug fields posts]} taxon]
+    [:<>
+     [:h1 (:title fields)]
+     [:div [:code slug]]
+     (map
+       (fn [{:post/keys [slug fields]}]
+         [:article
+          [:h2 (:title fields)]])
+       posts)]))
+
+(defc page [{:keys [post i18n menus] :as data}]
+  {:extends main-layout
+   :query [{:post/fields [:field/key :field/content]}]
+   :key :post}
+  (let [post (post/compact-fields post)
+        {:keys [title simple flex-content]} (:post/fields post)]
+    [:<>
+     [:h1 title]
+     [:h2 (:hello simple)]
+     [:p (:body simple)]
+     [:p.goodbye (:goodbye simple)]
+     [:p.flex flex-content]]))
+
+(defc static-page [{:keys [post lang]}]
+  {:key :post}
+  [:html {:lang lang}
+   [:head
+    [:title (first (:title post))]
+    [:meta {:charset "utf-8"}]]
+   [:body
+    [:main
+     [:div (when (:html post)
+             {:dangerouslySetInnerHTML
+              {:__html (:html post)}})]]]])
+
+(defc ^:not-found not-found [{:keys [i18n lang]}]
+  {}
+  ;; TODO extract this to a layout
+  [:html {:lang lang}
+   [:head
+    [:title (:not-found i18n)]
+    [:meta {:charset "utf-8"}]]
+   [:body
+    [:div (:not-found i18n)]]])
