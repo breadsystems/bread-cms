24a860cc4b7d5beb71cc6a2440addc0119f91b78
Coby Tamayo <coby@tamayo.email>
2023-02-06 00:45:28 -0800

    correctly infer taxon post queries

--- test/systems/bread/alpha/taxon_test.clj
+++ test/systems/bread/alpha/taxon_test.clj
@@ -32,12 +32,9 @@
         :query/db db
         :query/args
         ['{:find [(pull ?e0 [:db/id :taxon/slug]) .]
-           :in [$ % ?status ?taxonomy ?slug]
-           :where [[?e0 :taxon/slug ?slug]
-                   [?p :post/status ?status]
-                   (post-taxonomized ?p ?taxonomy ?slug)]}
-         [taxon/post-taxonomized-rule]
-         :post.status/published
+           :in [$ ?taxonomy ?slug]
+           :where [[?e0 :taxon/taxonomy ?taxonomy]
+                   [?e0 :taxon/slug ?slug]]}
          :taxon.taxonomy/category
          "some-tag"]}
        {:query/name ::taxon/compact
@@ -55,12 +52,9 @@
         :query/db db
         :query/args
         ['{:find [(pull ?e0 [:db/id :taxon/whatever]) .]
-           :in [$ % ?status ?taxonomy ?slug]
-           :where [[?e0 :taxon/slug ?slug]
-                   [?p :post/status ?status]
-                   (post-taxonomized ?p ?taxonomy ?slug)]}
-         [taxon/post-taxonomized-rule]
-         :post.status/published
+           :in [$ ?taxonomy ?slug]
+           :where [[?e0 :taxon/taxonomy ?taxonomy]
+                   [?e0 :taxon/slug ?slug]]}
          :taxon.taxonomy/tag
          "some-tag"]}
        {:query/name ::taxon/compact
@@ -70,6 +64,27 @@
        :dispatcher/key :tag
        :route/params {:lang "en" :slug "some-tag"}}
 
+      ;; {:uri "/en/tag/some-tag"}
+      ;; :post/type and :post/status have no effect without :post/_taxons
+      [{:query/name ::store/query
+        :query/key :tag
+        :query/db db
+        :query/args
+        ['{:find [(pull ?e0 [:db/id :taxon/whatever]) .]
+           :in [$ ?taxonomy ?slug]
+           :where [[?e0 :taxon/taxonomy ?taxonomy]
+                   [?e0 :taxon/slug ?slug]]}
+         :taxon.taxonomy/tag
+         "some-tag"]}
+       {:query/name ::taxon/compact
+        :query/key :tag}]
+      {:dispatcher/type :dispatcher.type/tag
+       :dispatcher/pull [:taxon/whatever]
+       :dispatcher/key :tag
+       :post/status :post.status/draft
+       :post/type :post.type/article
+       :route/params {:lang "en" :slug "some-tag"}}
+
       ;; {:uri "/en/by-taxon/category/some-tag"}
       ;; Query includes :taxon/field as a map.
       [{:query/name ::store/query
@@ -77,12 +92,9 @@
         :query/db db
         :query/args
         ['{:find [(pull ?e0 [:db/id :taxon/slug :taxon/fields]) .]
-           :in [$ % ?status ?taxonomy ?slug]
-           :where [[?e0 :taxon/slug ?slug]
-                   [?p :post/status ?status]
-                   (post-taxonomized ?p ?taxonomy ?slug)]}
-         [taxon/post-taxonomized-rule]
-         :post.status/published
+           :in [$ ?taxonomy ?slug]
+           :where [[?e0 :taxon/taxonomy ?taxonomy]
+                   [?e0 :taxon/slug ?slug]]}
          :taxon.taxonomy/category
          "some-tag"]}
        {:query/name ::store/query
@@ -106,92 +118,180 @@
        :route/params {:lang "en" :slug "some-tag"}}
 
       ;; {:uri "/en/tag/some-tag"}
-      ;; :dispatcher.type/tag with :post/type
+      ;; Default :post/type and :post/status with :post/_taxons
       [{:query/name ::store/query
         :query/key :tag
         :query/db db
         :query/args
-        ['{:find [(pull ?e0 [:db/id :taxon/whatever]) .]
-           :in [$ % ?status ?type ?taxonomy ?slug]
-           :where [[?e0 :taxon/slug ?slug]
-                   [?p :post/status ?status]
-                   [?p :post/type ?type]
-                   (post-taxonomized ?p ?taxonomy ?slug)]}
-         [taxon/post-taxonomized-rule]
-         :post.status/published
-         :post.type/page
+        ['{:find [(pull ?e0 [:db/id
+                             ;; :taxon/posts
+                             :post/_taxons]) .]
+           :in [$ ?taxonomy ?slug]
+           :where [[?e0 :taxon/taxonomy ?taxonomy]
+                   [?e0 :taxon/slug ?slug]]}
          :taxon.taxonomy/tag
          "some-tag"]}
+       {:query/name ::store/query
+        :query/key [:tag :post/_taxons]
+        :query/db db
+        :query/args
+        ['{:find [(pull ?post [:db/id :post/fields])]
+           :in [$ ?taxon ?type ?status]
+           :where [[?post :post/taxons ?taxon]
+                   [?post :post/type ?type]
+                   [?post :post/status ?status]]}
+         :post.type/page
+         :post.status/published
+         [::bread/data :tag :db/id]]}
+       {:query/name ::store/query
+        :query/key [:tag :post/_taxons :post/fields]
+        :query/db db
+        :query/args
+        ['{:find [(pull ?e [:db/id :field/key :field/content])]
+           :in [$ ?e1 ?lang]
+           :where [[?e :field/lang ?lang]
+                   [?e0 :post/fields ?e]
+                   [?e0 :post/taxons ?e1]]}
+         [::bread/data :tag :post/_taxons :db/id]
+         :en]}
        {:query/name ::taxon/compact
         :query/key :tag}]
       {:dispatcher/type :dispatcher.type/tag
-       :dispatcher/pull [:taxon/whatever]
+       :dispatcher/pull [{:post/_taxons [{:post/fields [:field/key
+                                                        :field/content]}]}]
        :dispatcher/key :tag
-       :post/type :post.type/page
        :route/params {:lang "en" :slug "some-tag"}}
 
       ;; {:uri "/en/tag/some-tag"}
-      ;; :dispatcher.type/tag with :post/status
+      ;; Custom :post/type and :post/status with :post/_taxons
       [{:query/name ::store/query
         :query/key :tag
         :query/db db
         :query/args
-        ['{:find [(pull ?e0 [:db/id :taxon/whatever]) .]
-           :in [$ % ?status ?taxonomy ?slug]
-           :where [[?e0 :taxon/slug ?slug]
-                   [?p :post/status ?status]
-                   (post-taxonomized ?p ?taxonomy ?slug)]}
-         [taxon/post-taxonomized-rule]
-         :post.status/draft
+        ['{:find [(pull ?e0 [:db/id
+                             ;; :taxon/posts
+                             :post/_taxons]) .]
+           :in [$ ?taxonomy ?slug]
+           :where [[?e0 :taxon/taxonomy ?taxonomy]
+                   [?e0 :taxon/slug ?slug]]}
          :taxon.taxonomy/tag
          "some-tag"]}
+       {:query/name ::store/query
+        :query/key [:tag :post/_taxons]
+        :query/db db
+        :query/args
+        ['{:find [(pull ?post [:db/id :post/fields])]
+           :in [$ ?taxon ?type ?status]
+           :where [[?post :post/taxons ?taxon]
+                   [?post :post/type ?type]
+                   [?post :post/status ?status]]}
+         :post.type/article
+         :post.status/draft
+         [::bread/data :tag :db/id]]}
+       {:query/name ::store/query
+        :query/key [:tag :post/_taxons :post/fields]
+        :query/db db
+        :query/args
+        ['{:find [(pull ?e [:db/id :field/key :field/content])]
+           :in [$ ?e1 ?lang]
+           :where [[?e :field/lang ?lang]
+                   [?e0 :post/fields ?e]
+                   [?e0 :post/taxons ?e1]]}
+         [::bread/data :tag :post/_taxons :db/id]
+         :en]}
        {:query/name ::taxon/compact
         :query/key :tag}]
       {:dispatcher/type :dispatcher.type/tag
-       :dispatcher/pull [:taxon/whatever]
+       :dispatcher/pull [{:post/_taxons [{:post/fields [:field/key
+                                                        :field/content]}]}]
        :dispatcher/key :tag
        :post/status :post.status/draft
+       :post/type :post.type/article
        :route/params {:lang "en" :slug "some-tag"}}
 
       ;; {:uri "/en/tag/some-tag"}
-      ;; :dispatcher.type/tag with :post/status nil
+      ;; :dispatcher.type/tag with :post/type and :post/status nil
       [{:query/name ::store/query
         :query/key :tag
         :query/db db
         :query/args
-        ['{:find [(pull ?e0 [:db/id :taxon/whatever]) .]
-           :in [$ % ?taxonomy ?slug]
-           :where [[?e0 :taxon/slug ?slug]
-                   (post-taxonomized ?p ?taxonomy ?slug)]}
-         [taxon/post-taxonomized-rule]
+        ['{:find [(pull ?e0 [:db/id
+                             ;; :taxon/posts
+                             :post/_taxons]) .]
+           :in [$ ?taxonomy ?slug]
+           :where [[?e0 :taxon/taxonomy ?taxonomy]
+                   [?e0 :taxon/slug ?slug]]}
          :taxon.taxonomy/tag
          "some-tag"]}
+       {:query/name ::store/query
+        :query/key [:tag :post/_taxons]
+        :query/db db
+        :query/args
+        ['{:find [(pull ?post [:db/id :post/fields])]
+           :in [$ ?taxon]
+           :where [[?post :post/taxons ?taxon]]}
+         [::bread/data :tag :db/id]]}
+       {:query/name ::store/query
+        :query/key [:tag :post/_taxons :post/fields]
+        :query/db db
+        :query/args
+        ['{:find [(pull ?e [:db/id :field/key :field/content])]
+           :in [$ ?e1 ?lang]
+           :where [[?e :field/lang ?lang]
+                   [?e0 :post/fields ?e]
+                   [?e0 :post/taxons ?e1]]}
+         [::bread/data :tag :post/_taxons :db/id]
+         :en]}
        {:query/name ::taxon/compact
         :query/key :tag}]
       {:dispatcher/type :dispatcher.type/tag
-       :dispatcher/pull [:taxon/whatever]
+       :dispatcher/pull [{:post/_taxons [{:post/fields [:field/key
+                                                        :field/content]}]}]
        :dispatcher/key :tag
+       :post/type nil
        :post/status nil
        :route/params {:lang "en" :slug "some-tag"}}
 
       ;; {:uri "/en/tag/some-tag"}
-      ;; :dispatcher.type/tag with :post/status false
+      ;; :dispatcher.type/tag with :post/type and :post/status false
       [{:query/name ::store/query
         :query/key :tag
         :query/db db
         :query/args
-        ['{:find [(pull ?e0 [:db/id :taxon/whatever]) .]
-           :in [$ % ?taxonomy ?slug]
-           :where [[?e0 :taxon/slug ?slug]
-                   (post-taxonomized ?p ?taxonomy ?slug)]}
-         [taxon/post-taxonomized-rule]
+        ['{:find [(pull ?e0 [:db/id
+                             ;; :taxon/posts
+                             :post/_taxons]) .]
+           :in [$ ?taxonomy ?slug]
+           :where [[?e0 :taxon/taxonomy ?taxonomy]
+                   [?e0 :taxon/slug ?slug]]}
          :taxon.taxonomy/tag
          "some-tag"]}
+       {:query/name ::store/query
+        :query/key [:tag :post/_taxons]
+        :query/db db
+        :query/args
+        ['{:find [(pull ?post [:db/id :post/fields])]
+           :in [$ ?taxon]
+           :where [[?post :post/taxons ?taxon]]}
+         [::bread/data :tag :db/id]]}
+       {:query/name ::store/query
+        :query/key [:tag :post/_taxons :post/fields]
+        :query/db db
+        :query/args
+        ['{:find [(pull ?e [:db/id :field/key :field/content])]
+           :in [$ ?e1 ?lang]
+           :where [[?e :field/lang ?lang]
+                   [?e0 :post/fields ?e]
+                   [?e0 :post/taxons ?e1]]}
+         [::bread/data :tag :post/_taxons :db/id]
+         :en]}
        {:query/name ::taxon/compact
         :query/key :tag}]
       {:dispatcher/type :dispatcher.type/tag
-       :dispatcher/pull [:taxon/whatever]
+       :dispatcher/pull [{:post/_taxons [{:post/fields [:field/key
+                                                        :field/content]}]}]
        :dispatcher/key :tag
+       :post/type false ; same as explicit nil.
        :post/status false ; same as explicit nil.
        :route/params {:lang "en" :slug "some-tag"}}
 
@@ -203,12 +303,10 @@
         :query/db db
         :query/args
         ['{:find [(pull ?e0 [:db/id :taxon/whatever]) .]
-           :in [$ % ?status ?type ?taxonomy ?slug]
-           :where [[?e0 :taxon/slug ?slug]
-                   (post-taxonomized ?p ?taxonomy ?slug)]}
-         [taxon/post-taxonomized-rule]
+           :in [$ ?status ?taxonomy ?slug]
+           :where [[?e0 :taxon/taxonomy ?taxonomy]
+                   [?e0 :taxon/slug ?slug]]}
          :post.status/draft ; populated from req
-         :post.type/article ; populated from req
          :taxon.taxonomy/tag
          "some-tag"]}
        {:query/name ::taxon/compact
