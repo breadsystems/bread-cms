0ba3e9a9c0b101d7a0134f57e287504dbe3241ce
Coby Tamayo <coby@tamayo.email>
2023-10-13 08:45:43 -0700

    support building & deploying plugin jars

--- build.clj
+++ build.clj
@@ -13,10 +13,9 @@
 
 (def minor-version "0.7")
 
-(def lib 'systems.bread/bread-core)
+(def core-lib 'systems.bread/bread-core)
 (def patch-version (format "%s.%s" minor-version (b/git-count-revs nil)))
 (def class-dir "target/classes")
-(def jar-file (format "target/%s-%s.jar" (name lib) patch-version))
 (def uber-file (format "target/bread-%s-standalone.jar" patch-version))
 
 (defn clean [_]
@@ -25,18 +24,62 @@
 (defn tag [_]
   (print (str "v" patch-version)))
 
+(def libs
+  {:core
+   {:lib 'systems.bread/bread-core}
+
+   :datahike
+   {:lib 'systems.bread/bread-plugin-datahike
+    :aliases [:datahike]}
+
+   :garden
+   {:lib 'systems.bread/bread-plugin-garden
+    :aliases [:garden]}
+
+   :markdown
+   {:lib 'systems.bread/bread-plugin-markdown
+    :aliases [:markdown]}
+
+   :reitit
+   {:lib 'systems.bread/bread-plugin-reitit
+    :aliases [:reitit]}
+
+   :rum
+   {:lib 'systems.bread/bread-plugin-rum
+    :aliases [:rum]}
+
+   :selmer
+   {:lib 'systems.bread/bread-plugin-selmer
+    :aliases [:selmer]}
+
+   ;;
+   })
+
+(defn- jar-path [lib version]
+  (format "target/%s-%s.jar" (name lib) version))
+
 ;; TODO parameterize to build CMS & plugins.
 ;; This only builds core for now.
-(defn jar [_]
-  (b/write-pom {:class-dir class-dir
-                :lib lib
-                :version patch-version
-                :basis (b/create-basis {:project "deps.edn"})
-                :src-dirs ["src"]})
-  (b/copy-dir {:src-dirs ["src"]
-               :target-dir class-dir})
-  (b/jar {:class-dir class-dir
-          :jar-file jar-file}))
+(defn jar [opts]
+  (let [{:keys [aliases lib]} (get libs (:lib opts :core))
+        jar-file (jar-path lib patch-version)]
+    (clean nil)
+    (b/write-pom {:class-dir class-dir
+                  :lib lib
+                  :version patch-version
+                  :basis (b/create-basis {:project "deps.edn"
+                                          :aliases aliases})
+                  :src-dirs ["src"]
+                  :pom-data
+                  [:licenses
+                   [:license
+                    [:name "Anti-Capitalist Software License v1.4"]
+                    [:url "https://anticapitalist.software/"]]]})
+    (b/copy-dir {:src-dirs ["src"]
+                 :target-dir class-dir})
+    (println "Writing jar:" jar-file)
+    (b/jar {:class-dir class-dir
+            :jar-file jar-file})))
 
 (defn uber [_]
   (println "Cleaning target directory...")
@@ -58,7 +101,10 @@
              :main 'systems.bread.alpha.cms.main}))
   (println "Uberjar written to" uber-file))
 
-(defn deploy [_]
-  (dd/deploy {:installer :remote
-              :artifact jar-file
-              :pom-file (b/pom-path {:lib lib :class-dir class-dir})}))
+(defn deploy [opts]
+  (let [{:keys [lib]} (get libs (:lib opts :core))
+        jar-file (jar-path lib patch-version)]
+    (println "Deploying jar:" jar-file)
+    (dd/deploy {:installer :remote
+                :artifact jar-file
+                :pom-file (b/pom-path {:lib lib :class-dir class-dir})})))
