7ade0b048b7236290918e13969a829d6e47c4de2
Coby Tamayo <coby@tamayo.email>
2024-02-11 11:42:04 -0800

    fix post-ancestry rule

--- test/core/systems/bread/alpha/post_test.clj
+++ test/core/systems/bread/alpha/post_test.clj
@@ -16,14 +16,14 @@
 
     '[(post-ancestry ?child ?slug_0)
       [?child :post/slug ?slug_0]
-      (not-join [?child] [?_ :post/parent ?child])]
+      (not-join [?child] [?_ :post/children ?child])]
     [1]
 
     '[(post-ancestry ?child ?slug_0 ?slug_1)
       [?child :post/slug ?slug_0]
       [?ancestor_1 :post/children ?child]
       [?ancestor_1 :post/slug ?slug_1]
-      (not-join [?ancestor_1] [?_ :post/parent ?ancestor_1])]
+      (not-join [?ancestor_1] [?_ :post/children ?ancestor_1])]
     [2]
 
     '[(post-ancestry ?child ?slug_0 ?slug_1 ?slug_2)
@@ -32,7 +32,7 @@
       [?ancestor_1 :post/slug ?slug_1]
       [?ancestor_2 :post/children ?ancestor_1]
       [?ancestor_2 :post/slug ?slug_2]
-      (not-join [?ancestor_2] [?_ :post/parent ?ancestor_2])]
+      (not-join [?ancestor_2] [?_ :post/children ?ancestor_2])]
     [3]
 
     '[(post-ancestry ?child ?slug_0 ?slug_1 ?slug_2 ?slug_3)
@@ -43,7 +43,7 @@
       [?ancestor_2 :post/slug ?slug_2]
       [?ancestor_3 :post/children ?ancestor_2]
       [?ancestor_3 :post/slug ?slug_3]
-      (not-join [?ancestor_3] [?_ :post/parent ?ancestor_3])]
+      (not-join [?ancestor_3] [?_ :post/children ?ancestor_3])]
     [4]
 
     '[(post-ancestry ?child ?slug_0 ?slug_1 ?slug_2 ?slug_3 ?slug_4)
@@ -56,7 +56,7 @@
       [?ancestor_3 :post/slug ?slug_3]
       [?ancestor_4 :post/children ?ancestor_3]
       [?ancestor_4 :post/slug ?slug_4]
-      (not-join [?ancestor_4] [?_ :post/parent ?ancestor_4])]
+      (not-join [?ancestor_4] [?_ :post/children ?ancestor_4])]
     [5]))
 
 (deftest test-dispatch-post-queries
