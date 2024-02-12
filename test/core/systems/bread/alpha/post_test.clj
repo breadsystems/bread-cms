e28b405897c53774b18d2ba3c452f9caf5922b91
Coby Tamayo <coby@tamayo.email>
2024-02-11 23:50:03 -0800

    simplify post tests

--- test/core/systems/bread/alpha/post_test.clj
+++ test/core/systems/bread/alpha/post_test.clj
@@ -11,20 +11,20 @@
 
 (deftest test-create-post-ancestry-rule
   (are
-    [rule args]
-    (= rule (apply post/create-post-ancestry-rule args))
+    [rule n]
+    (= rule (post/create-post-ancestry-rule n))
 
     '[(post-ancestry ?child ?slug_0)
       [?child :post/slug ?slug_0]
       (not-join [?child] [?_ :post/children ?child])]
-    [1]
+    1
 
     '[(post-ancestry ?child ?slug_0 ?slug_1)
       [?child :post/slug ?slug_0]
       [?ancestor_1 :post/children ?child]
       [?ancestor_1 :post/slug ?slug_1]
       (not-join [?ancestor_1] [?_ :post/children ?ancestor_1])]
-    [2]
+    2
 
     '[(post-ancestry ?child ?slug_0 ?slug_1 ?slug_2)
       [?child :post/slug ?slug_0]
@@ -33,7 +33,7 @@
       [?ancestor_2 :post/children ?ancestor_1]
       [?ancestor_2 :post/slug ?slug_2]
       (not-join [?ancestor_2] [?_ :post/children ?ancestor_2])]
-    [3]
+    3
 
     '[(post-ancestry ?child ?slug_0 ?slug_1 ?slug_2 ?slug_3)
       [?child :post/slug ?slug_0]
@@ -44,7 +44,7 @@
       [?ancestor_3 :post/children ?ancestor_2]
       [?ancestor_3 :post/slug ?slug_3]
       (not-join [?ancestor_3] [?_ :post/children ?ancestor_3])]
-    [4]
+    4
 
     '[(post-ancestry ?child ?slug_0 ?slug_1 ?slug_2 ?slug_3 ?slug_4)
       [?child :post/slug ?slug_0]
@@ -57,7 +57,7 @@
       [?ancestor_4 :post/children ?ancestor_3]
       [?ancestor_4 :post/slug ?slug_4]
       (not-join [?ancestor_4] [?_ :post/children ?ancestor_4])]
-    [5]))
+    5))
 
 (deftest test-dispatch-post-queries
   (let [db ::FAKEDB
