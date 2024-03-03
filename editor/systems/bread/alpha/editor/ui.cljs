685d79fc5631d351826e98b70d0919a1bdec24f1
Coby Tamayo <coby@tamayo.email>
2024-03-03 14:08:01 -0800

    icons PoC

--- editor/systems/bread/alpha/editor/ui.cljs
+++ editor/systems/bread/alpha/editor/ui.cljs
@@ -1,21 +1,38 @@
 (ns systems.bread.alpha.editor.ui
   (:require
-    [rum.core :as rum]))
+    [rum.core :as rum]
+    [systems.bread.alpha.editor.internal.macros
+     :include-macros true :refer [i]]))
 
 ;; TODO
 (def t {:media-library "Media Library"
         :save "Save"
         :settings "Settings"})
 
+(defmulti tool-label identity)
+
+(defmethod tool-label :default [kw]
+  (clojure.string/upper-case (str (first (name kw)))))
+
+(defmethod tool-label :blockquote [_] (i :Editor :double-quotes-l))
+#_(defmethod tool-label :ul         [_] (-> tiptap .chain .focus .toggleBulletList .run))
+#_(defmethod tool-label :ol         [_] (-> tiptap .chain .focus .toggleOrderedList .run))
+#_(defmethod tool-label :strike     [_] (-> tiptap .chain .focus .toggleStrike .run))
+#_(defmethod tool-label :highlight  [_] (-> tiptap .chain .focus .toggleHighlight .run))
+#_(defmethod tool-label :sub        [_] (-> tiptap .chain .focus .toggleSubscript .run))
+#_(defmethod tool-label :sup        [_] (-> tiptap .chain .focus .toggleSuperscript .run))
+#_(defmethod tool-label :code       [_] (-> tiptap .chain .focus .toggleCode .run))
+#_(defmethod tool-label :codeblock  [_] (-> tiptap .chain .focus .toggleCodeBlock .run))
+#_(defmethod tool-label :br         [_] (-> tiptap .chain .focus .setHardBreak .run))
+#_(defmethod tool-label :hr         [_] (-> tiptap .chain .focus .setHorizontalRule .run))
+
 (rum/defc EditorMenu < rum/reactive [toolbar config]
   [:div {:data-bread-menu true
          :data-field-name (:name config)
          :data-field-type (:type config)}
    (map (fn [{:keys [tool effect]}]
-          (let [label (if (map? tool)
-                        (or (:label tool) (:type tool))
-                        tool)]
-            [:button {:key tool :on-click effect} (str label)])) (:tools toolbar))])
+          (let [tk (if (map? tool) (:type tool) tool)]
+            [:button {:key tool :on-click effect} (tool-label tk)])) (:tools toolbar))])
 
 (defn menu-element [{:keys [id]}]
   (let [elem (js/document.createElement "DIV")]
