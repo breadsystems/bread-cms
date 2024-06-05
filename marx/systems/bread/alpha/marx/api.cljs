f6d20da99bf56b65e8a5567f2756fb883f520a44
Coby Tamayo <coby@tamayo.email>
2024-06-05 02:46:22 -0700

    React PoC

--- marx/systems/bread/alpha/marx/api.cljs
+++ marx/systems/bread/alpha/marx/api.cljs
@@ -1,9 +1,12 @@
 (ns systems.bread.alpha.marx.api
   (:require
-    [systems.bread.alpha.marx.core :as core]))
+    [systems.bread.alpha.marx.core :as core]
+    ["/MarxEditor$default" :as MarxEditor]
+    ["react" :as react]
+    ["react-dom/client" :as rdom]))
 
 (defn from-meta-element
-  ([editor-name]
+    ([editor-name]
    (let [selector (str "meta[name=\"" (name editor-name) "\"]")]
      (core/read-attr (js/document.querySelector selector) "content")))
   ([]
@@ -14,8 +17,22 @@
          :marx/fields {}
          :marx/listeners))
 
+(def ^:private read-attr (memoize core/read-attr))
+
+(defn mount! [ed elem]
+  (let [field (read-attr elem "data-marx")
+        field-name (:name field)
+        !root (or
+                (get-in @ed [:marx/fields field-name :root])
+                (get-in (swap! ed assoc-in [:marx/fields field-name]
+                               (assoc field
+                                      :elem elem
+                                      :root (rdom/createRoot elem)))
+                        [:marx/fields field-name :root]))]
+    (.render !root (MarxEditor #js {:children (js/Array.from (.-children elem))}))))
+
 (defn init! [ed {:keys [attr]
                  :or {attr "data-marx"}}]
   (let [elements (vec (js/document.querySelectorAll (str "[" attr "]")))]
     (doseq [elem elements]
-      (prn 'found elem))))
+      (mount! ed elem))))
