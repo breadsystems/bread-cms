0a2da7b58472f028e46105a7c054d267c56ca18c
Coby Tamayo <coby@tamayo.email>
2024-06-05 10:57:18 -0700

    always assoc :elem in field config

--- marx/systems/bread/alpha/marx/api.cljs
+++ marx/systems/bread/alpha/marx/api.cljs
@@ -35,8 +35,9 @@
                 kvs)))
 
 (defmethod core/init-field! :rich-text
-  [ed field elem]
-  (let [tools (or (:tools field)
+  [ed field]
+  (let [elem (:elem field)
+        tools (or (:tools field)
                   (:tools ed)
                   tiptap/default-rich-text-tools)
         menu-elem (doto
@@ -60,8 +61,9 @@
     (.removeChild (.-parentNode elem) elem)))
 
 (defmethod core/init-field! :editor-bar
-  [ed field elem]
-  (let [field-name (:name field)
+  [ed field]
+  (let [elem (:elem field)
+        field-name (:name field)
         !root (or
                 (get-in @ed [:marx/fields field-name :root])
                 (get-in (persist-field! ed field elem
@@ -73,9 +75,10 @@
 (defn init-field! [ed config {:keys [elem initialized?] :as field}]
   (let [field (if initialized?
                 field
-                (read-attr elem (:attr config "data-marx")))]
+                (assoc (read-attr elem (:attr config "data-marx"))
+                       :elem elem))]
     (prn 'init field)
-    (core/init-field! ed field elem)))
+    (core/init-field! ed field)))
 
 (defn init! [ed {:keys [attr]
                  :or {attr "data-marx"}
@@ -85,6 +88,6 @@
                  (doto (map
                    (fn [elem] {:elem elem})
                    (vec (js/document.querySelectorAll (str "[" attr "]")))) (prn 2)))]
-    (prn 'fields (map (juxt :name :initialized?) fields))
+    (prn 'fields (map (juxt :name :initialized? :elem) fields))
     (doseq [field fields]
       (init-field! ed config field))))
