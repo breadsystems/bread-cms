3901d657f558687e9a8c983a64910fb269780b61
Coby Tamayo <coby@tamayo.email>
2024-06-05 09:42:29 -0700

    MOUNT TIPTAP!

--- marx/systems/bread/alpha/marx/api.cljs
+++ marx/systems/bread/alpha/marx/api.cljs
@@ -1,9 +1,13 @@
 (ns systems.bread.alpha.marx.api
   (:require
-    [systems.bread.alpha.marx.core :as core]
-    ["/MarxEditor$default" :as MarxEditor]
     ["react" :as react]
-    ["react-dom/client" :as rdom]))
+    ["react-dom/client" :as rdom]
+    ["@tiptap/core" :refer [Editor] :rename {Editor TiptapEditor}]
+
+    ["/MarxEditor$default" :as MarxEditor]
+    ["/EditorBar$default" :as EditorBar]
+    [systems.bread.alpha.marx.core :as core]
+    [systems.bread.alpha.marx.tiptap :as tiptap]))
 
 (defn from-meta-element
     ([editor-name]
@@ -13,26 +17,71 @@
    (from-meta-element "marx-editor")))
 
 (defn editor [config]
-  (assoc config
-         :marx/fields {}
-         :marx/listeners))
+  (assoc config :marx/fields {}))
 
-(def ^:private read-attr (memoize core/read-attr))
+(defn elements [ed]
+  (map :elem (vals (:marx/fields ed))))
 
-(defn mount! [ed elem]
-  (let [field (read-attr elem "data-marx")
-        field-name (:name field)
+(defn field [ed field-name]
+  (get-in ed [:marx/fields field-name]))
+
+(def read-attr (memoize core/read-attr))
+
+(defn- persist-field! [ed field elem & kvs]
+  (swap! ed assoc-in [:marx/fields (:name field)]
+         (apply assoc field
+                :elem elem
+                :persisted? true
+                kvs)))
+
+(defn- recreate-tiptap [])
+
+(defmethod core/init-field! :rich-text
+  [ed field elem]
+  (let [tools (or (:tools field)
+                  (:tools ed)
+                  tiptap/default-rich-text-tools)
+        menu-elem (doto
+                    (js/document.createElement "DIV")
+                    (.setAttribute "id" "editor-menu"))
+        extensions (tiptap/extensions ed tools
+                                      {:menu-element menu-elem})
+        ;; TODO conditionally instantiate TiptapEditor
+        tiptap (TiptapEditor. (clj->js {:element (.-parentNode elem)
+                                        :extensions extensions
+                                        :content (.-outerHTML elem)}))]
+    (persist-field! ed field elem :tiptap tiptap :menu-elem menu-elem)
+    ;; TODO createRoot
+    (.render (rdom/createRoot menu-elem) (EditorBar))
+    (prn 'tiptap (:tiptap field))
+    (.removeChild (.-parentNode elem) elem)))
+
+(defmethod core/init-field! :editor-bar
+  [ed field elem]
+  (let [field-name (:name field)
         !root (or
                 (get-in @ed [:marx/fields field-name :root])
-                (get-in (swap! ed assoc-in [:marx/fields field-name]
-                               (assoc field
-                                      :elem elem
-                                      :root (rdom/createRoot elem)))
+                (get-in (persist-field! ed field elem
+                                        :root (rdom/createRoot elem))
                         [:marx/fields field-name :root]))]
-    (.render !root (MarxEditor #js {:children (js/Array.from (.-children elem))}))))
+    (prn 'render field elem)
+    (.render !root (EditorBar #js {:children (js/Array.from (.-children elem))}))))
+
+(defn init-field! [ed config {:keys [elem persisted?] :as field}]
+  (let [field (if persisted?
+                field
+                (read-attr elem (:attr config "data-marx")))]
+    (prn 'init field)
+    (core/init-field! ed field elem)))
 
 (defn init! [ed {:keys [attr]
-                 :or {attr "data-marx"}}]
-  (let [elements (vec (js/document.querySelectorAll (str "[" attr "]")))]
-    (doseq [elem elements]
-      (mount! ed elem))))
+                 :or {attr "data-marx"}
+                 :as config}]
+  (let [fields (or
+                 (vals (:marx/fields @ed))
+                 (map
+                   (fn [elem] {:elem elem})
+                   (vec (js/document.querySelectorAll (str "[" attr "]")))))]
+    (prn 'fields (map (juxt :name :persisted?) fields))
+    (doseq [field fields]
+      (init-field! ed config field))))
