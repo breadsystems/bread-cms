bc615b64aa81385590d7035c5dedcbb072c73541
Coby Tamayo <coby@tamayo.email>
2024-06-08 15:11:13 -0700

    implement theme infra

---
+++ marx/components/theme.js
@@ -0,0 +1,62 @@
+/* CSS HEX
+--lavender-pink: #f0a6caff;
+--lavender-blush: #f0e6efff;
+--tiffany-blue: #7cdedcff;
+--glaucous: #7284a8ff;
+--charcoal: #474954ff;
+ */
+const solarizedDarkPalette = {
+  base03:  '#002b36',
+  base02:  '#073642',
+  base01:  '#586e75',
+  base00:  '#657b83',
+  base0:   '#839496',
+  base1:   '#93a1a1',
+  base2:   '#eee8d5',
+  base3:   '#fdf6e3',
+  yellow:  '#b58900',
+  orange:  '#cb4b16',
+  red:     '#dc322f',
+  magenta: '#d33682',
+  violet:  '#6c71c4',
+  blue:    '#268bd2',
+  cyan:    '#2aa198',
+  green:   '#859900',
+};
+const solarizedLightPalette = {
+  // TODO
+};
+
+const baseTheme = {
+  type: {
+    font_heading: 'serif',
+    font_body: 'sans-serif',
+  },
+  spacing: {
+    gap_small: '0.25em',
+  },
+};
+
+const darkTheme = {
+  ...baseTheme,
+  color: {
+    text: solarizedDarkPalette.base3,
+    background: solarizedDarkPalette.base03,
+  },
+};
+
+const lightTheme = {
+  ...baseTheme,
+  color: {
+    text_main: '#171818',
+    background_main: '#e3d8a2',
+    accent_main: '#ce7575',
+  },
+};
+
+export {
+  solarizedDarkPalette,
+  solarizedLightPalette,
+  darkTheme,
+  lightTheme,
+};
