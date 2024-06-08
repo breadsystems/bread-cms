2661117c0c395cd410a949d139efb2dc1c9518cd
Coby Tamayo <coby@tamayo.email>
2024-06-08 16:31:32 -0700

    export everything from index.js

---
+++ marx/components/index.js
@@ -0,0 +1,13 @@
+export {BreadBar} from './BreadBar';
+export {BreadStyle} from './BreadStyle';
+export {Button} from './Button';
+export {EditorMenu} from './EditorMenu';
+export {Popover} from './Popover';
+export {SettingsBox} from './SettingsBox';
+export {Spacer} from './Spacer';
+export {
+  solarizedDarkPalette,
+  solarizedLightPalette,
+  darkTheme,
+  lightTheme,
+} from './theme';
