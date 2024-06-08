fe24ea57de6214588cd29e9b47f64f57784d53a4
Coby Tamayo <coby@tamayo.email>
2024-06-07 17:53:59 -0700

    get SettingsBox rendering in Popover

--- marx/components/Popover.jsx
+++ marx/components/Popover.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
+import styled from 'styled-components';
+import * as Rp from '@radix-ui/react-popover';
 
-function Popover({trigger, content}) {
-  return <div>
-    {trigger}
-  </div>;
+const PopoverContent = styled.div`
+  --bar-height: 1em; // TODO
+
+  position: absolute;
+
+  [data-radix-popper-content-wrapper] {
+    position: absolute !important;
+    transform: translate(0, calc(-1 * (var(--bar-height) + 100%))) !important;
+    transform-origin: bottom left;
+  }
+`;
+
+function Popover({trigger, children}) {
+  const [open, setOpen] = useState(false);
+
+  return <Rp.Root open={open}>
+    {open && (
+      <PopoverContent>
+        <Rp.Content>
+          {children}
+        </Rp.Content>
+      </PopoverContent>
+    )}
+    <Rp.Trigger asChild onClick={() => setOpen(!open)}>
+      {trigger}
+    </Rp.Trigger>
+  </Rp.Root>;
 }
 
 export {Popover};
