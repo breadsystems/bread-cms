f78585060fc29d677f6eba2f3d34ef2331f91ae1
Coby Tamayo <coby@tamayo.email>
2024-06-08 23:57:53 -0700

    configure palette

--- marx/js/BreadStyle.js
+++ marx/js/BreadStyle.js
@@ -5,12 +5,22 @@ const BreadStyle = createGlobalStyle`
     box-sizing: border-box;
   }
   :root {
-    --marx-color-text-main: ${({theme}) => theme.color.text_main};
-    --marx-color-bg-main: ${({theme}) => theme.color.background_main};
-    --marx-color-accent-main: ${({theme}) => theme.color.accent_main};
+    --marx-color-text-main:        ${({theme}) => theme.color.text_main};
+    --marx-color-text-dark:        ${({theme}) => theme.color.text_dark};
+    --marx-color-text-desaturated: ${({theme}) => theme.color.text_desaturated};
 
-    --marx-border-style-box: ${({theme}) => theme.border.box_style};
-    --marx-border-width-box: ${({theme}) => theme.border.box_width};
+    --marx-color-background-main:        ${({theme}) => theme.color.background_main};
+    --marx-color-background-dark:        ${({theme}) => theme.color.background_dark};
+    --marx-color-background-desaturated: ${({theme}) => theme.color.background_desaturated};
+
+    --marx-color-accent-main:        ${({theme}) => theme.color.accent_main};
+    --marx-color-accent-detail:      ${({theme}) => theme.color.accent_detail};
+    --marx-color-accent-dark:        ${({theme}) => theme.color.accent_dark};
+    --marx-color-accent-desaturated: ${({theme}) => theme.color.accent_desaturated};
+
+    --marx-border-width:       ${({theme}) => theme.border.width};
+    --marx-border-style-box:   ${({theme}) => theme.border.style_box};
+    --marx-border-style-input: ${({theme}) => theme.border.style_input};
   }
 `;
 
