c10972bfa547d7504964e17cec63f570e134011f
Coby Tamayo <coby@tamayo.email>
2024-06-09 00:07:18 -0700

    mv rich-text into its own ns

--- marx/systems/bread/alpha/marx/api.cljs
+++ marx/systems/bread/alpha/marx/api.cljs
@@ -1,13 +1,10 @@
 (ns systems.bread.alpha.marx.api
   (:require
     ["react" :as react]
-    ["react-dom/client" :as rdom]
-    ["@tiptap/core" :refer [Editor] :rename {Editor TiptapEditor}]
 
     [systems.bread.alpha.marx.field.bar :as bar]
-    ["/EditorMenu" :refer [EditorMenu]]
-    [systems.bread.alpha.marx.core :as core]
-    [systems.bread.alpha.marx.tiptap :as tiptap]))
+    [systems.bread.alpha.marx.field.rich-text]
+    [systems.bread.alpha.marx.core :as core]))
 
 (defn from-meta-element
     ([editor-name]
@@ -21,38 +18,6 @@
 
 (defn elements [ed]
   (map :elem (vals (:marx/fields ed))))
-
-(defmethod core/tool-props :bold [ed tool]
-  {:tooltip "Bold"
-   :icon "bold"})
-
-(defmethod core/field-lifecycle :rich-text
-  [ed {:keys [state elem] :as field}]
-  (let [tools (or (:tools field) (:tools ed) tiptap/default-rich-text-tools)]
-    {:init-state
-     (fn []
-       (let [menu-elem (js/document.createElement "DIV")
-             extensions (tiptap/extensions ed tools {:menu-element menu-elem})]
-         {:menu-elem menu-elem
-          :menu-root (rdom/createRoot menu-elem)
-          :extensions extensions
-          :tiptap (TiptapEditor. (clj->js {:element (.-parentNode elem)
-                                           :extensions extensions
-                                           :content (.-outerHTML elem)}))}))
-     :did-mount
-     (fn [state]
-       (.removeChild (.-parentNode elem) elem))
-     :render
-     (fn [{:keys [menu-root tiptap]}]
-       (let [menu-tool (fn [tool]
-                         (let [tk (if (map? tool) (:type tool) tool)
-                               props {:type tk
-                                      :content (name tk)
-                                      :effect #(tiptap/command tiptap tool)}]
-                           (merge props (core/tool-props @ed props))))
-             menu-props {:tools (map menu-tool tools)}]
-         (.render menu-root (EditorMenu (clj->js menu-props))))
-       nil)}))
 
 (def bar-section bar/bar-section)
 
