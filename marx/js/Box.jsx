c3673bc1624ec9585b36a1b667d8a27de1e6a21c
Coby Tamayo <coby@tamayo.email>
2024-06-09 02:12:52 -0700

    refine dark theme

--- marx/js/Box.jsx
+++ marx/js/Box.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Aside = styled.aside`
+  color: var(--marx-color-text-main);
   background-color: var(--marx-color-background-slab);
   padding: 1em;
   border: var(--marx-border-box);
