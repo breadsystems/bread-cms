5df61d9d15f98b850e273ccdb7d4044cfc49d9e2
Coby Tamayo <coby@tamayo.email>
2024-06-08 23:58:08 -0700

    style buttons!

--- marx/stories/Button.stories.jsx
+++ marx/stories/Button.stories.jsx
@@ -1,16 +1,31 @@
-import { Button } from '../js/Button';
+import {userEvent, within} from '@storybook/test';
+
+import {Button} from '../js/Button';
 
 const meta = {
   component: Button,
   parameters: {
     layout: 'centered',
   },
+  args: {
+    children: 'Button',
+  },
 };
 
 export default meta;
 
-export const Base = {
+export const Base = {};
+
+export const Disabled = {
   args: {
-    children: 'Button',
+    disabled: true,
+  },
+};
+
+export const Focused = {
+  play: async ({canvasElement}) => {
+    const canvas = within(canvasElement);
+    const button = canvas.getByText('Button');
+    button.focus();
   },
 };
