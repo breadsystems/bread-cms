7433eebdc08543793ce5c680dfe1f223264b01ec
Coby Tamayo <coby@tamayo.email>
2024-06-09 01:31:14 -0700

    extract Box component

--- marx/js/MediaLibrary.jsx
+++ marx/js/MediaLibrary.jsx
@@ -3,12 +3,13 @@ import styled from 'styled-components';
 
 import {Box} from './Box';
 
-function SettingsBox() {
+function MediaLibrary({settings}) {
+  console.log('settings', settings);
   return <Box>
-    <h2>SETTINGS GO HERE</h2>
+    <h2>MEDIAZ GO HERE</h2>
     <section>Section one</section>
     <section>Section two</section>
   </Box>;
 }
 
-export {SettingsBox};
+export {MediaLibrary};
