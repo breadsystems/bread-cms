4bf4d5abe543c0515119b1afbdb4580428a662e7
Coby Tamayo <coby@tamayo.email>
2024-06-13 01:54:13 -0700

    create nix dev env

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "The Bread development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells."${system}".default = let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in pkgs.mkShell {
+        packages = with pkgs; [
+          nodejs_22
+          clojure
+          zulu11
+          babashka
+        ];
+      };
+    };
+}
