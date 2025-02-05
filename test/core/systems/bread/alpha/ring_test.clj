a0103cb86424b4dfa4a805da321e2c176d56e402
Coby Tamayo <coby@tamayo.email>
2025-02-04 22:00:56 -0800

    disallow open redirects

--- test/core/systems/bread/alpha/ring_test.clj
+++ test/core/systems/bread/alpha/ring_test.clj
@@ -8,7 +8,7 @@
   (are
     [expected-res res action]
     (= expected-res (-> res
-                        (merge {::bread/hooks {::bread/render [action]}})
+                        (update ::bread/hooks merge {::bread/render [action]})
                         (bread/hook ::bread/render)
                         (select-keys [:status :headers :flash :session])))
 
@@ -27,6 +27,48 @@
      :headers {"X-Marco" :polo}}
     {:action/name ::ring/redirect
      :to "/destination"}
+
+    {:status 302
+     :headers {"Content-Type" "text/html"
+               "Location" "/somewhere"}
+     :session {:user {:user/username "bob"}}}
+    {:status 404
+     :session {:user {:user/username "bob"}}
+     :headers {"Content-Type" "text/html"}}
+    {:action/name ::ring/redirect
+     :to "/somewhere"}
+
+    ;; Disallow open redirects.
+    {:status 200
+     :headers {"Content-Type" "text/html"}
+     :session {:user {:user/username "bob"}}}
+    {:status 200
+     :session {:user {:user/username "bob"}}
+     :headers {"Content-Type" "text/html"}}
+    {:action/name ::ring/redirect
+     :to "evil.com/somewhere"}
+
+    {:status 200
+     :headers {"Content-Type" "text/html"}
+     :session {:user {:user/username "bob"}}}
+    {:status 200
+     :session {:user {:user/username "bob"}}
+     :headers {"Content-Type" "text/html"}}
+    {:action/name ::ring/redirect
+     :to "http://evil.com/somewhere"}
+
+    {:status 302
+     :headers {"Content-Type" "text/html"
+               "Location" "http://careful.xxx/sus"}
+     :session {:user {:user/username "bob"}}}
+    {:status 200
+     :session {:user {:user/username "bob"}}
+     :headers {"Content-Type" "text/html"}
+     ::bread/hooks {::ring/allow-redirect?
+                    [{:action/name ::bread/value
+                      :action/value true}]}}
+    {:action/name ::ring/redirect
+     :to "http://careful.xxx/sus"}
 
     ;;
     ))
