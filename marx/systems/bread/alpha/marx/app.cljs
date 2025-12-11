a042e74ac6d420b10655b9fa8efe99a8b0c6547f
Coby Tamayo <coby@tamayo.email>
2025-12-10 22:08:24 -0800

    read from dynamic metadata

--- marx/systems/bread/alpha/marx/app.cljs
+++ marx/systems/bread/alpha/marx/app.cljs
@@ -3,52 +3,21 @@
     ["/theme" :refer [darkTheme lightTheme]]
     [systems.bread.alpha.marx.api :as marx]))
 
-(defonce my-editor
-  (let [config (marx/from-meta-element)]
-    (atom (marx/editor {;; TODO get actual config from <meta>
-                        :name :my-editor
-                        :site/name "Sandbox"
-                        :site/settings {:bar/position :bottom
-                                        :theme/variant :dark}
-                        :theme/variants #js {:dark darkTheme
-                                             :light lightTheme}
-                        :backend
-                        {:type :bread/http
-                         :endpoint "/~/edit"}
-                        #_
-                        {:type :bread/websocket
-                         :uri "ws://localhost:13120/_bread"}
-                        #_#_
-                        :collab {:strategy :webrtc
-                                 :ydoc ydoc
-                                 :provider
-                                 (WebrtcProvider.
-                                   "breadroom" ydoc
-                                   (clj->js {:signaling
-                                             ["ws://localhost:4444"]}))
-                                 #_#_
-                                 :user (:collab/user config)
-                                 :user {:name (rand-nth ["cobby"
-                                                         "rown"
-                                                         "yous"
-                                                         "joshy"
-                                                         "willy"
-                                                         "jacko"])
-                                        :color (rand-nth ["red"
-                                                          "blue"
-                                                          "green"
-                                                          "grey"
-                                                          "brown"
-                                                          "yellow"])
-                                        :data-avatar "/cat.jpg"}}}))))
+(defonce ed
+  (let [config (merge (marx/from-meta-element)
+                      {:theme/variants #js {:dark darkTheme
+                                            :light lightTheme}})]
+    ;; TODO debug logging
+    (prn 'config config)
+    (atom (marx/editor config))))
 
 (comment
-  (deref my-editor)
-  (marx/elements @my-editor)
+  (deref ed)
+  (marx/elements @ed)
   (js/location.reload))
 
 (defn ^:dev/after-load start []
-  (marx/init! my-editor {}))
+  (marx/init! ed {}))
 
 (defn init []
   (start))
