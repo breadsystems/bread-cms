f96d1e3249ce96266ed8f766df2e5e71dd3f2062
Coby Tamayo <coby@tamayo.email>
2026-08-08 17:27:08 -0700

    implement markdown plugin!

--- plugins/markdown/systems/bread/alpha/plugin/markdown.cljc
+++ plugins/markdown/systems/bread/alpha/plugin/markdown.cljc
@@ -6,7 +6,10 @@
     [clojure.string :as string]
     [clojure.java.io :as io]
     [markdown.core :as md]
-    [systems.bread.alpha.core :as bread]))
+    [systems.bread.alpha.core :as bread]
+    [systems.bread.alpha.i18n :as i18n])
+  (:import
+    #?(:clj [java.io File])))
 
 (comment
 
@@ -98,6 +101,7 @@
       (fn [path _]
         {:uri (abs-path->uri path dir ext)})))
 
+#_
 (defn plugin
   ([]
    (plugin {}))
@@ -118,3 +122,63 @@
        :static/slug-param slug-param
        :static/parse parse
        :static/parse-meta? parse-meta?}})))
+
+;; TODO rm ^
+
+(defmethod bread/action ::singularize-metadata
+  [{:as req :keys [::bread/dispatcher]} _action [content]]
+  (let [configured? (bread/config req :markdown/singularize-metadata?)
+        singularize? (bread/hook req ::singularize-metadata?
+                                 (:singularize-metadata? dispatcher configured?))]
+    (if singularize?
+      (update content :metadata #(into {} (map (juxt key (comp first val)) %)))
+      content)))
+
+(defmethod bread/expand ::markdown
+  [{:keys [filepaths hook]} _]
+  (let [file (loop [[filepath & filepaths] filepaths]
+               (let [file (io/resource filepath)]
+                 (cond
+                   file file
+                   (seq filepaths) (recur filepaths))))]
+    (when file
+      (hook ::html (md/md-to-html-string-with-meta (slurp file)) file))))
+
+(defmethod bread/dispatch ::markdown=>
+  [{:as req dispatcher ::bread/dispatcher}]
+  (let [params (:route/params dispatcher)
+        slug (get params (bread/config req :markdown/slug-param))
+        extensions (bread/config req :markdown/extensions)
+        paths (bread/config req :markdown/paths)
+        index-filename (bread/config req :markdown/index-filename)
+        ->path (fn [& path-components]
+                 (let [path-components (filter identity path-components)]
+                   (string/join (File/separator) (map name path-components))))
+        filepaths (if slug
+                    (for [path paths ext extensions]
+                      (->path path (i18n/lang req) (str slug ext)))
+                    (for [path paths]
+                      (->path path (i18n/lang req) index-filename)))]
+    {:expansions [{:expansion/name ::markdown
+                   :expansion/key (:dispatcher/key dispatcher :markdown)
+                   :filepaths (bread/hook req ::filepaths filepaths params)
+                   :hook (partial bread/hook req)}]}))
+
+(defn plugin
+  ([]
+   (plugin {}))
+  ([{:keys [paths extensions index-filename singularize-metadata? slug-param]
+     :or {paths ["public"]
+          extensions [".md"]
+          index-filename "index.md"
+          singularize-metadata? true
+          slug-param :slug}}]
+   {:config {:markdown/paths paths
+             :markdown/extensions extensions
+             :markdown/index-filename index-filename
+             :markdown/singularize-metadata? singularize-metadata?
+             :markdown/slug-param slug-param}
+    :hooks
+    {::html
+     [{:action/name ::singularize-metadata
+       :action/description "Singularize metadata from a markdown file."}]}}))
--- test/cms/systems/bread/alpha/markdown_test.clj
+++ test/cms/systems/bread/alpha/markdown_test.clj
@@ -2,6 +2,9 @@
   (:require
     [clojure.test :refer [are deftest]]
     [markdown.core :as md]
+
+    [systems.bread.alpha.core :as bread]
+    [systems.bread.alpha.test-helpers :refer [plugins->loaded]]
     [systems.bread.alpha.plugin.markdown :as markdown]))
 
 (deftest test-query-fs
@@ -107,6 +110,111 @@
                                   :path->req
                                   [0 1 2]}]
     ))
+
+(deftest singularize-metadata-hook
+  (let [metadata* {:a [:first :second :third]
+                   :b [0 1 2]}]
+    (are
+      [expected markdown-config dispatcher hooks]
+      (= expected (let [app (plugins->loaded [(markdown/plugin markdown-config)
+                                              {:hooks hooks}])
+                        req (assoc app ::bread/dispatcher dispatcher)
+                        content {:metadata metadata*}
+                        result (bread/hook req ::markdown/html content)]
+                    (:metadata result)))
+
+      {:a :first :b 0}
+      {}
+      {:dispatcher/type ::markdown/markdown=>}
+      nil
+
+      {:a :first :b 0}
+      {:singularize-metadata? true}
+      {:dispatcher/type ::markdown/markdown=>}
+      nil
+
+      metadata*
+      {:singularize-metadata? false}
+      {:dispatcher/type ::markdown/markdown=>}
+      nil
+
+      metadata*
+      {:singularize-metadata? nil}
+      {:dispatcher/type ::markdown/markdown=>}
+      nil
+
+      {:a :first :b 0}
+      {:singularize-metadata? false}
+      {:dispatcher/type ::markdown/markdown=>
+       :singularize-metadata? true}
+      nil
+
+      metadata*
+      {:singularize-metadata? true}
+      {:dispatcher/type ::markdown/markdown=>
+       :singularize-metadata? false}
+      nil
+
+      metadata*
+      {:singularize-metadata? true}
+      {:dispatcher/type ::markdown/markdown=>
+       :singularize-metadata? true}
+      {::markdown/singularize-metadata? [{:action/name ::bread/value
+                                          :action/value false}]}
+
+      {:a :first :b 0}
+      {:singularize-metadata? false}
+      {:dispatcher/type ::markdown/markdown=>}
+      {::markdown/singularize-metadata? [{:action/name ::bread/value
+                                          :action/value true}]}
+
+      ,)))
+
+(deftest test-markdown-expansion
+  (let [mock-fs
+        {"public/en/page.md"   "Markdown doc in English under /public"
+         "public/en/meta.md"   "Title: Whoa, Meta!\n\nDoc with metadata"}]
+    (with-redefs [clojure.java.io/resource str
+                  slurp mock-fs]
+      (are
+        [expected expansion]
+        (= expected (bread/expand expansion {}))
+
+        nil
+        {:expansion/name ::markdown/markdown
+         :filepaths ["non-existent/file/path"]
+         :hook (partial bread/hook {})}
+
+        nil
+        {:expansion/name ::markdown/markdown
+         :filepaths ["non-existent/file/path" "another/non-existent/path"]
+         :hook (partial bread/hook {})}
+
+        {:metadata nil
+         :html "<p>Markdown doc in English under /public</p>"}
+        {:expansion/name ::markdown/markdown
+         :filepaths ["public/en/page.md"]
+         :hook (partial bread/hook {})}
+
+        {:metadata {:title ["Whoa, Meta!"]}
+         :html "<p>Doc with metadata</p>"}
+        {:expansion/name ::markdown/markdown
+         :filepaths ["public/en/meta.md"]
+         :hook (partial bread/hook {::bread/hooks
+                                    {::markdown/html
+                                     [{:action/name ::markdown/singularize-metadata}]}})}
+
+        {:metadata {:title "Whoa, Meta!"}
+         :html "<p>Doc with metadata</p>"}
+        {:expansion/name ::markdown/markdown
+         :filepaths ["public/en/meta.md"]
+         :hook (partial bread/hook {::bread/config
+                                    {:markdown/singularize-metadata? true}
+                                    ::bread/hooks
+                                    {::markdown/html
+                                     [{:action/name ::markdown/singularize-metadata}]}})}
+
+        ,))))
 
 (comment
   (require '[kaocha.repl :as k])
